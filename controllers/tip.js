51391499e56cd58f894b7e7eff38fe5f523f9d56
dguerrab <d.guerrab@alumnos.upm.es>
2018-06-10 22:23:10 +0200

    Working Tips

--- controllers/tip.js
+++ controllers/tip.js
@@ -20,13 +20,12 @@ exports.load = (req, res, next, tipId) => {
 
 // POST /quizzes/:quizId/tips
 exports.create = (req, res, next) => {
-    const authorId = req.session.user && req.session.user.id || 0;
-
+    //const authorId = req.session.user && req.session.user.id || 0;
     const tip = models.tip.build(
         {
             text: req.body.text,
             quizId: req.quiz.id,
-            authorId: authorId
+            authorId: req.session.user && req.session.user.id
         });
 
     tip.save({fields: ["text", "quizId", "authorId"]})
